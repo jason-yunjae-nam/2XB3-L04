8918f0c114f918d7d931e6312f8303a8b08bad1f
Cassie <baldic1@mcmaster.ca>
2021-01-20 18:24:22 -0500

    Added function are_valid_groups

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
-aa
+def are_valid_groups(st_nums, groups):
+    count = 0
+    for num in st_nums:
+        for group in groups:
+            for i in group:
+                if num == i:
+                    count = count + 1
+                    break
+                    
+    if count == len(st_nums):
+        return True
+    return False
