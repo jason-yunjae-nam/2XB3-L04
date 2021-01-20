e5611cf13faae5479bc2efd486247875aa97a470
aami-hus <hussaa54@mcmaster.ca>
2021-01-20 18:25:05 -0500

    are_valid_groups aa.ver

--- code.py
+++ code.py
@@ -1,1 +1,12 @@
-aa
+def are_valid_groups(num, groups):
+    counter = 0
+    i = 0
+    while i < len(num):
+        for j in range(len(groups)):
+            counter += groups[j].count(num[i])
+        if counter == 0:
+            return False
+        i+=1
+        counter=0
+    else:
+        return True
