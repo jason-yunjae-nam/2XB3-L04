16ed5bc02feffe24aeaf35fec474a501dc064f1c
jason-yunjae-nam <55263909+jason-yunjae-nam@users.noreply.github.com>
2021-01-20 18:50:53 -0500

    .\code.py

--- code.py
+++ code.py
@@ -1,1 +1,9 @@
-aa
+def are_valid_groups(studentnumbers, groups):
+    counts = 0
+    for i in range(len(groups)):
+        for j in range(len(studentnumbers)):
+            counts += groups[i].count(studentnumbers[j])
+    if counts == 0:
+        return False
+    else:
+        return True
