f3eb90fdaeb2bb1435012c918325c0a1f9a1f2ad
jason-yunjae-nam <55263909+jason-yunjae-nam@users.noreply.github.com>
2021-01-20 19:30:54 -0500

    messed up code.py

--- code.py
+++ code.py
@@ -1,12 +1,10 @@
 def are_valid_groups(num, groups):
     counter = 0
-    i = 0
-    while i < len(num):
-        for j in range(len(groups)):
-            counter += groups[j].count(num[i])
-        if counter == 0:
-            return False
+asdfasdfasf    
+while i < len(num):
+good evening            
+counter += groups[j].count(num[i])
         i+=1
         counter=0
     else:
-        return True
+hello
