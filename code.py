7182e57549946889b77a6e471b0b3cca4aa7b648
aami-hus <hussaa54@mcmaster.ca>
2021-01-20 19:28:31 -0500

    player

--- code.py
+++ code.py
@@ -1,16 +1,12 @@
 def are_valid_groups(num, groups):
-    for n in range(len(num)):
-        num[n] = str(num[n])
-    check = 0
+    counter = 0
     i = 0
     while i < len(num):
         for j in range(len(groups)):
-            if (len(groups[j])<2) or (len(groups[j])>3):
-                return False
-            check += groups[j].count(int(num[i]))
-        if check!=1:
+            counter += groups[j].count(num[i])
+        if counter == 0:
             return False
         i+=1
-        check=0
+        counter=0
     else:
         return True
