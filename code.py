ab81e22d675847a3f3c9d8e6f6977d110278f314
aami-hus <hussaa54@mcmaster.ca>
2021-01-20 19:15:58 -0500

    final ver are_valid_group

--- code.py
+++ code.py
@@ -1,14 +1,3 @@
-<<<<<<< HEAD
-def are_valid_groups(studentnumbers, groups):
-    counts = 0
-    for i in range(len(groups)):
-        for j in range(len(studentnumbers)):
-            counts += groups[i].count(studentnumbers[j])
-    if counts == 0:
-        return False
-    else:
-        return True
-=======
 def are_valid_groups(num, groups):
     counter = 0
     i = 0
@@ -21,4 +10,3 @@ def are_valid_groups(num, groups):
         counter=0
     else:
         return True
->>>>>>> c5e96d5ecbe5fbeae9a5a9c0e8a4316a4084c335
