4badf24ce50a8288167cca2518919d777e7b6e0d
aami-hus <hussaa54@mcmaster.ca>
2021-01-27 18:25:44 -0500

    initial time test fxn for copy() created

--- lab2/code.py
+++ lab2/code.py
@@ -1,5 +1,18 @@
-def test_list_copy():
-    thislist = []
-    newlist = thislist.copy
+import timeit
+
+def test_list_copy(n):
+    thislist = list(range(1,n))
+    newlist = thislist.copy()
     return newlist
 
+def timetest(runs, n):
+    total = 0
+    for _ in range(runs):
+        start = timeit.default_timer()
+        test_list_copy(n)
+        end = timeit.default_timer()
+        total += end - start
+    return total/runs
+
+for i in range(1, 1000):
+    print(i*100, timetest(100, i*100))
