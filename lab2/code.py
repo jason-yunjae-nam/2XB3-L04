63bd25f76043036198ed5f3970a2fd4264e2f21f
aami-hus <hussaa54@mcmaster.ca>
2021-01-27 19:10:19 -0500

    added lookup fxn and time test for lookup

--- lab2/code.py
+++ lab2/code.py
@@ -5,7 +5,7 @@ def test_list_copy(n):
     newlist = thislist.copy()
     return newlist
 
-def timetest(runs, n):
+def timetest_copy(runs, n):
     total = 0
     for _ in range(runs):
         start = timeit.default_timer()
@@ -14,5 +14,26 @@ def timetest(runs, n):
         total += end - start
     return total/runs
 
+'''
 for i in range(1, 1000):
-    print(i*100, timetest(100, i*100))
+    print(i*100, timetest_copy(100, i*100))'''
+
+#####
+
+def test_list_lookup():
+    thislist = list(range(1000000))
+    for i in range(len(thislist)):
+        thislist[i]
+
+def timetest_lookup(runs):
+    total = 0
+    for _ in range(runs):
+        start = timeit.default_timer()
+        test_list_lookup()
+        end = timeit.default_timer()
+        total += end - start
+    return total/runs
+
+
+for i in range(1, 1000):
+    print(i, timetest_lookup(10))
