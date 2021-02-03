b022bd3b1cf649b7975296c9e20b969df90a3727
aami-hus <hussaa54@mcmaster.ca>
2021-02-03 10:33:02 -0500

    added quicksort inplace fxn

--- lab3/sorts.py
+++ lab3/sorts.py
@@ -1,5 +1,23 @@
 def quicksort_inplace(L):
-    return
+    if len(L) < 2:
+        return L
+    pivot = L[len(L)-1]
+    left = -1
+    right = 0
+    for _ in range(len(L)-1):
+        if L[right] > pivot:
+            right+=1
+        else:
+            left+=1
+            L[left], L[right] = L[right], L[left]
+            right+=1
+    L[left+1], L[len(L)-1] = L[len(L)-1], L[left+1]
+    return quicksort_inplace(L[:left+1]) + [L[left+1]] + quicksort_inplace(L[left+2:])
+
+#lst = [33,89,55,35,45,98,76,54,49]
+lst = [43, 93, 98,69,76,32,97,31]
+print(quicksort_inplace(lst))
+    
 
 """
 def my_quicksort(L):
